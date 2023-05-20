e8a530d09a6411a3e5b7cdebae51d1c2c9fd2202
tharunsri <89070175+tharunsri@users.noreply.github.com>
2023-05-20 11:27:41 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,78 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6">
+          <div class="footer-info">
+            <h3>Blood Donate Management System</h3>
+            <p>
+              Sardar Patel Road <br>
+              Anna University, Chennai-25<br><br>
+              <strong>Phone:</strong> +91 9360873454<br>
+              <strong>Email:</strong> donationblood819@gmail.com<br>
+            </p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Pages</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Gallery</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Register/Login</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Agent Login</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Events</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="partials/locations.php">Locate Us</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">FAQs</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Host a Blood Drive</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Help Center</a></li>
+          </ul>
+        </div>
+
+        
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="copyright">
+      &copy; 2023 <strong><span>Blood Donation Management System</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      It's not how much we give, but how much love we put into giving.
+    </div>
+  </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
